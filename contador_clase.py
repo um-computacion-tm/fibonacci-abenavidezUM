3ebe467a03c7eec8bfb5ace8c8a5e298b2f01bde
abenavidezUM <a.benavidez@alumno.um.edu.ar>
2023-05-16 12:51:11 -0300

    Ejercicio contador de palabras

--- contador_clase.py
+++ contador_clase.py
@@ -1,15 +1,38 @@
-#Clase 04/04
-
 import unittest
 
-def count_words(word):
-    return {word: 1}
+def contador(sentence):
+    
+    palabras = sentence.split()
+    npalabras = len(palabras)
+    return npalabras
 
-class TestCountLetters(unittest.TestCase):
- 
-    def test_simple(self):
-        result = count_words('hola')
-        self.assertEqual(result, {'hola': 1})
+
+class TestWordCounter(unittest.TestCase):
+    
+    def test_0(self):
+        result = contador('')
+        self.assertEqual(result, 0)
         
+    def test_1(self):
+        result = contador('test')
+        self.assertEqual(result, 1)
+        
+    def test_2(self):
+        result = contador('segundo test')
+        self.assertEqual(result, 2)
+        
+    def test_3(self):
+        result = contador('..test..')
+        self.assertEqual(result, 1)
+        
+    def test_4(self):
+        result = contador('..test.. cuatro!')
+        self.assertEqual(result, 2)
+        
+    def test_5(self):
+        result = contador('Este es mi ultimo test')
+        self.assertEqual(result, 5)
+
+
 if __name__ == '__main__':
     unittest.main()
